d838da1da3113c48d959fe317ec9b52254f63049
Nabadarshi Chakma <nabadarshi@faqinteractive.com.au>
2025-11-06 18:52:43 +0600

    WIP - new changes

---
+++ back-end/models/User.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+};
+
+const UserSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: [/.+\@.+\..+/, "Please enter a valid email address"]
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, "Password must be at least 6 characters long"]
+  },
+  role: {
+    type: String,
+    enum: ["customer", "admin"],
+    default: "customer",
+  },
+}, {
+  timestamps: true
+});
+
+// Password Hash Middleware
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Mtach 
