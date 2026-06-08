fe9bd4cbf3e483266155840c44973043aee6052f
Nabadarshi Chakma <nabadarshi@faqinteractive.com.au>
2026-06-08 18:56:27 +0600

    WIP - Adding debugger.

--- front-end/src/components/cart/pages/Checkout.tsx
+++ front-end/src/components/cart/pages/Checkout.tsx
@@ -5,11 +5,14 @@ import { TypeShippingAddress } from "../types";
 import { useHookForm } from "../../libs/HookForm";
 import { Form } from "../../form/Form";
 import { Input } from "../../form/Input";
+import { useState } from "react";
+import { SubmitButton } from "../../form/SubmitButton";
 
 const Checkout = () => {
 
+  const [checkoutId, setCheckoutId] = useState<string | null>(null);
   const { initialState, names, labels } = composeInitialState<TypeShippingAddress>({
-    email: "",
+    // email: "",
     firstName: "",
     lastName: "",
     address: "",
@@ -20,7 +23,7 @@ const Checkout = () => {
   });
 
   const schema = Yup.object<TypeShippingAddress>().shape({
-    email: Yup.string().email().required().label(labels.email),
+    // email: Yup.string().email().required().label(labels.email),
     firstName: Yup.string().required().label(labels.firstName),
     lastName: Yup.string().required().label(labels.lastName),
     address: Yup.string().required().label(labels.address),
@@ -39,21 +42,23 @@ const Checkout = () => {
     <div className="checkout-container">
       {/* Left Section */}
       <div className="tw:rounded-lg tw:p-6">
-        <h2 className="tw:text-2xl tw:uppercase tw:mb-6">Checkout</h2>
+        <h2 className="tw:!text-2xl tw:uppercase tw:!mb-6">Checkout</h2>
         <Form
           methods={methods}
           onSubmit={() => { }}
         // className="tw:max-w-md tw:p-8 tw:rounded-lg tw:border-gray-500 tw:shadow-sm"
         >
           <div className="row">
+            <h3 className="tw:!text-lg tw:!mb-4">Contact Details</h3>
             <div className="col-12">
               <Input
-                name={names.email}
-                label={labels.email}
+                name=""
+                value="abc@example.com"
+                label="Email"
                 disabled
               />
             </div>
-            <h3 className="tw:text-lg tw:mb-4">Delivery</h3>
+            <h3 className="tw:!text-lg tw:!mb-4">Delivery</h3>
             <div className="col-md-6">
               <Input
                 name={names.firstName}
@@ -68,6 +73,53 @@ const Checkout = () => {
                 required
               />
             </div>
+            <div className="col-12">
+              <Input
+                name={names.address}
+                label={labels.address}
+                required
+              />
+            </div>
+            <div className="col-md-6">
+              <Input
+                name={names.city}
+                label={labels.city}
+                required
+              />
+            </div>
+            <div className="col-md-6">
+              <Input
+                name={names.postalCode}
+                label={labels.postalCode}
+                required
+              />
+            </div>
+            <div className="col-12">
+              <Input
+                name={names.country}
+                label={labels.country}
+                required
+              />
+            </div>
+            <div className="col-12">
+              <Input
+                name={names.phone}
+                label={labels.phone}
+                required
+              />
+            </div>
+            {
+              !checkoutId
+                ? <div className="col-12">
+                  <SubmitButton
+                    className="tw:!bg-black tw:!w-full"
+                    label="Continue to Pyment"
+                  />
+                </div>
+                : <div>
+                  <h3 className="tw:!text-lg tw:!mb-4">Pay with paypal</h3>
+                </div>
+            }
           </div>
         </Form>
       </div>
